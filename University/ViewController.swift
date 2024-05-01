1367389b440c435142a5b149682dcfcf5c859342
Sharaf <sharafa839@gmail.com>
2024-05-02 01:30:07 +0300

    Initial Commit

---
+++ University/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  University
+//
+//  Created by Sharaf on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
